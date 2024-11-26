bb65746c7c79e1bb85dfc4b06868ff0e5041a371
abrham111 <abrhambayih733@gmail.com>
2024-11-26 11:16:49 +0300

    update notification

--- Frontend/src/pages/Notification.jsx
+++ Frontend/src/pages/Notification.jsx
@@ -1,22 +1,23 @@
 import { notifications } from "../constants/notifications";
 
 function Notification() {
+
   return (
-    <section id="Notification" className="bg-[#00000080] h-[600px] w-[65%] overflow-y-scroll">
+    <section id="Notification" className="bg-[#00000080] h-[600px] w-[65%] relative overflow-y-scroll rounded-lg border border-gray-300 shadow-md">
       <div className="text-center mb-3">
         <h1 className="font-semibold text-4xl">Notifications</h1>
       </div>
 
       <div className="flex flex-col">
         {notifications.map((notification) => (
-          <div className="flex flex-row" key={notification.id}>
+          <div className="flex flex-row gap-3" key={notification.id}>
             <img src={notification.profileImg} alt="ProfileImg" className="rounded-full" height={24} width={24} />
-            <div className="m-1.5">
+            <div className="mr-5 gap-3 w-[80%]">
               <h3 className="text-2xl">{notification.name}</h3>
-              <p>{notification.message}</p>
+              <p className="text-base text-gray leading-relaxed mb-3">{notification.message}</p>
             </div>
-            <div className="justify-end mt-2">
-              <p className="justify-end text-white">{notification.time}</p>
+            <div className="justify-end mt-2 ml-2">
+              <p className=" text-gray-300">{notification.time}</p>
             </div>
           </div>
         ))}
