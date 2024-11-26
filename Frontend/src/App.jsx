f581fa86aff713b54a88c83dc2a4008c2d288c06
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-26 10:47:39 +0300

    Login page merged

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { useState } from 'react';
 
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
@@ -12,19 +11,15 @@ import Notification from './pages/Notification';
 import Approvals from './pages/Approvals';
 
 function App() {
-  const [user, setUser] = useState(null);
+
   return (
     <BrowserRouter>
     <Routes>
     <Route path='/login' element={<Login/>}/>
       <Route path='/' element={<SharedPage/>}>
-        <Route index element={<Dashboard user={user}/>}/>
+        <Route index element={<Dashboard/>}/>
         <Route path='about' element={<About/>}/>
-<<<<<<< HEAD
-        <Route path='login' element={<Login setUser={setUser}/>}/>
-=======
         
->>>>>>> 62134c23d11922e4ad9444f9a6679acc7a56f05a
         <Route path='FAQs' element={<FAQs/>}/>
         <Route path='notification' element={<Notification/>}/>
         <Route path='approvals' element={<Approvals/>}/>
